5f54540b9ed4e50f724886841f1a7a889096c596
Kapil <kapil.pipaliya@yahoo.com>
2021-04-28 04:19:43 +0530

    add first_name and last_name to registraiton form

--- db/schema.rb
+++ db/schema.rb
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 2021_04_27_121446) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "first_name", default: "", null: false
+    t.string "last_name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
