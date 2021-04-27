5ac51b8f71ecd5684fb876e9aae7ff98c71ce534
Kapil <kapil.pipaliya@yahoo.com>
2021-04-27 23:23:20 +0530

    revert to commonjs format in environment.js

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,11 +1,11 @@
-import {environment} from '@rails/webpacker'
+const {environment} = require('@rails/webpacker')
 
-import {ProvidePlugin} from 'webpack'
+const webpack = require('webpack')
 
 environment.plugins.append('Provide',
-    new ProvidePlugin({
+    new webpack.ProvidePlugin({
         $: 'jquery',
         jQuery: 'jquery',
         Popper: ['popper.js', 'default']
     }))
-export default environment
+module.exports = environment
