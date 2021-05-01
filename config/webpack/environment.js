be878dfdd3bfd0ebc1b968ed765ac1b4910cd54c
Kapil <kapil.pipaliya@yahoo.com>
2021-05-01 09:06:46 +0530

    prevent jquery from loading

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -4,8 +4,8 @@ const webpack = require('webpack')
 
 environment.plugins.append('Provide',
     new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        Popper: ['popper.js', 'default']
+        // $: 'jquery',
+        // jQuery: 'jquery',
+        // Popper: ['popper.js', 'default']
     }))
 module.exports = environment
