2d99a5d26bcbabc828f926213e69ec5f2dd3278a
kapil <kapil.pipaliya@yahoo.com>
2021-05-27 12:54:45 +0530

    move spinner css to global css file

--- app/javascript/components/Spinner.tsx
+++ app/javascript/components/Spinner.tsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
 import clsx from 'clsx';
-import styles from './Spinner.module.scss';
 
 interface SpinnerProps {
   className?: string;
 }
 export const Spinner = (props: SpinnerProps) => {
   const { className } = props;
-  return <div className={clsx(styles.spinnerGrow, className)} role="status"></div>;
+  return <div className={clsx("spinner-grow", className)} role="status"></div>;
 };
